348565e42515aec7cff8c977f80c3fac24574169
gulzat.d <ms.gdlr223@gmail.com>
2023-12-19 09:45:00 +0500

    Точно рабочий

--- src/helpers/RequireAuth.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-
-export const RequireAuth = ({ children }: { children: ReactNode }) => {
-	if (condition) {
-
-	}
-	return children;
-}
--- src/hooks/use-login.hook.ts
+++
@@ -1,3 +0,0 @@
-// function useLogin(params) {
-
-// }
